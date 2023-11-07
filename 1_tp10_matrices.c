e60723a200ad0c6ff8509759d53d16d922cc87e0
Nayeli Cirilo <90116499+nayeli47@users.noreply.github.com>
2023-11-07 10:37:13 -0300

    Add files via upload

---
+++ 1_tp10_matrices.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void imprimir(int f){
+	
+	printf("\n la cantidad de ceros en la matriz es: %d\n",f);	
+	
+}
+int cont(int cant){
+	int mat[4][4]={{0},{0}},i,j;	
+	
+	srand(time(NULL));
+	for (i = 0; i < 4; i++) {
+		for(j=0;j<4;j++){
+			mat[i][j] = rand() % 10;
+			printf("%d\t",mat[i][j]);
+			
+			if(mat[i][j]==0){
+				cant++;
+			}
+		}
+		printf("\n");
+	}
+	return cant;
+}
+	
+	
+	int main() {
+		int cant=0,f=0;	
+		f=cont(cant);
+		imprimir(f);
+		return 0;
+	}
+	
+	
